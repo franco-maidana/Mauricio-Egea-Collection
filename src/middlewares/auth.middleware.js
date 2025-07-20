9b3262265e1d19bed749b01e970337fb0c2bcec4
Franco Maidana <francomaidana094@gmail.com>
2025-07-20 20:11:15 -0300

    se quito las funciones del auth.middlewares.js que ya no se usan y se modifico las rutas del user.api.router.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,20 +1,3 @@
-import * as userService from '../service/user.service.js';
-
-// Middleware para proteger rutas (autenticación)
-export const requireAuth = async (req, res, next) => {
-  try {
-    if (!req.session.userId) {
-      return res.status(401).json({ message: 'No autenticado' });
-    }
-    // Busca usuario real en la base de datos
-    const user = await userService.findUserById(req.session.userId);
-    if (!user) return res.status(401).json({ message: 'No autenticado' });
-    req.user = user; // el usuario ya viene "limpio" por tu service
-    next();
-  } catch (err) {
-    next(err);
-  }
-};
 
 // Middleware para proteger rutas (autenticación)
 export const requireAuthPassport = (req, res, next) => {
@@ -24,13 +7,6 @@ export const requireAuthPassport = (req, res, next) => {
   return res.status(401).json({ message: 'No autenticado' });
 };
 
-// Middleware para autorización por rol
-export const requireRole = (...roles) => (req, res, next) => {
-  if (!req.user || !roles.includes(req.user.role)) {
-    return res.status(403).json({ message: 'Acceso denegado - No tienes role suficiente para acceder a esta parte' });
-  }
-  next();
-};
 
 // Middleware para autorización por rol
 export const requireRolePassport = (...roles) => (req, res, next) => {
--- src/router/api/user.api.router.js
+++ src/router/api/user.api.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as userController from '../../controller/user.controller.js'
 import upload from '../../config/multer.js'
-import { requireAuth, requireRole } from '../../middlewares/auth.middleware.js';
+import { requireAuthPassport, requireRolePassport } from '../../middlewares/auth.middleware.js';
 
 
 const users = Router()
@@ -10,18 +10,18 @@ const users = Router()
 users.post('/create', upload.single('avatar'), userController.createUser);
 
 // 2. GET - http://localhost:8080/api/users/list
-users.get('/list', requireAuth, requireRole('admin'), userController.getUsers);
+users.get('/list', requireAuthPassport, requireRolePassport('admin'), userController.getUsers);
 
 //    GET - http://localhost:8080/api/users/list/:id
-users.get('/list/:id', requireAuth, requireRole('admin'), userController.getUserById);
+users.get('/list/:id', requireAuthPassport, requireRolePassport('admin'), userController.getUserById);
 
 //    GET - http://localhost:8080/api/users/email/:email
-users.get('/email/:email', requireAuth, requireRole('admin'), userController.getUserByEmail);
+users.get('/email/:email', requireAuthPassport, requireRolePassport('admin'), userController.getUserByEmail);
 
 // 3. PUT - http://localhost:8080/api/users/update/:id
-users.put('/update/:id', requireAuth, upload.single('avatar'), userController.updateUser);
+users.put('/update/:id', requireAuthPassport, upload.single('avatar'), userController.updateUser);
 
 // 4. DELETE - http://localhost:8080/api/users/destroi/:id
-users.delete('/destroi/:id', requireAuth, requireRole('admin'), userController.deleteUser);
+users.delete('/destroi/:id', requireAuthPassport, requireRolePassport('admin'), userController.deleteUser);
 
 export default users
